c21341b4806929883bebf6c3277e80a5c6268e2c
antoniolaikauf <antonio.laikauf00@gmail.com>
2023-11-27 13:36:24 +0100

    feat: aggiunto caratteristica per vedere chi vince con la scelta pari

--- pari e dispari/JS/index.js
+++ pari e dispari/JS/index.js
@@ -5,9 +5,11 @@ let sceltaNumero= parseInt(prompt("scegli numero tra 1 e 5"));
 // console.log(sceltaNumero);
 let choice = prompt("scegli tra pari o dispari");
 // console.log(choice);
+
 // variabili del valore massimo e minimo di scelta
 let max= 5;
 let min = 1;
+
 // creaione numero random
 function randomNumber() {
     let random = Math.floor(Math.random() * max) + min;
@@ -16,3 +18,18 @@ function randomNumber() {
 
 let numero = randomNumber();
 console.log(numero);
+
+// creazione funzione  calcolo
+function calcolo() {
+    let calcolo = sceltaNumero + numero;
+    return calcolo
+}
+let risultatoCalcolo = calcolo();
+console.log(risultatoCalcolo);
+
+// scelta per vedere risultato chi vince con il pari come scelta
+if (choice === "pari" && risultatoCalcolo % 2 === 0) {
+    console.log("utente ha vinto");
+}else{
+    console.log("utente ha perso");
+}
