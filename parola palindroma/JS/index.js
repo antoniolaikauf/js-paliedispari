f37cdcf04557b339370089b1bb63b69a20006576
antoniolaikauf <antonio.laikauf00@gmail.com>
2023-11-27 12:58:35 +0100

    feat: creazione dello script per vedere se parola è palindroma  o no

--- parola palindroma/JS/index.js
+++ parola palindroma/JS/index.js
@@ -4,8 +4,24 @@ const parola = prompt("inserisci parola");
 // console.log(parola);
 
 // verificare se parola inserita dall'utnte è palindroma o no
-
 function controllo() {
-    console.log(parola);
+    // mettere varibile inversa uguale a parola 
+    let inversa = parola
+    // dividere la parola e la mette in un array 
+    const divisioneString = inversa.split("")
+    console.log(divisioneString);
+    // invertire la parola ( inverte l'array)
+    const reverseString = divisioneString.reverse()
+    console.log(reverseString);
+    // riunire parola e toglierla dall'array
+    const unioneString = reverseString.join("")
+    console.log(unioneString);
+    // verificare se parola è palindroma o no
+    if ( parola === unioneString) {
+        console.log("è una parola palindroma");
+    }else{
+        console.log("non è una parola palindroma");
+    }
 }
 controllo();
+
