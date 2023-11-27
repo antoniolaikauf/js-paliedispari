f10c5a08669a73bb6ec4a232e9edf220ac4dfe24
antoniolaikauf <antonio.laikauf00@gmail.com>
2023-11-27 12:29:21 +0100

    feat: inserito richiesta per parola

---
+++ parola palindroma/JS/index.js
@@ -0,0 +1,11 @@
+// chiedere una parol all'utente
+
+const parola = prompt("inserisci parola");
+// console.log(parola);
+
+// verificare se parola inserita dall'utnte è palindroma o no
+
+function controllo() {
+    console.log(parola);
+}
+controllo();
