157e249248c1bbc46f14d1b507857397edee3eaf
antoniolaikauf <antonio.laikauf00@gmail.com>
2023-11-27 13:13:14 +0100

    feat: aggiunto richieste per utente

---
+++ pari e dispari/JS/index.js
@@ -0,0 +1,8 @@
+// chiedere a utente due prompt uno e per scegliere se pari o dispari 
+// l'altro è per il numero 
+
+let sceltaNumero= parseInt(prompt("scegli numero tra 1 e 5"));
+console.log(sceltaNumero);
+let choice = prompt("scegli tra pari o dispari");
+console.log(choice);
+
